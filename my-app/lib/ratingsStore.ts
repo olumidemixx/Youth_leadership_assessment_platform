7dfa8b3aa223c95386744b3bb0095f4fae9d5385
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-12-28 22:57:15 +0100

    latest2

--- my-app/lib/ratingsStore.ts
+++ my-app/lib/ratingsStore.ts
@@ -1,4 +1,4 @@
 // app/lib/ratingsStore.ts
 // lib/ratingsStore.ts
-export const ratingsStore: Record<string, number[][]> = {};
+export const ratingsStore: Record<string, number[]> = {};
 // Example: { "1": [q1, q2, q3, q4], "2": [q1, q2, q3, q4] }
